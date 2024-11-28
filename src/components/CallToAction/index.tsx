56e7d1f522382f84563f9cfa6ccbe308abf05db1
alexrobaina <alexrobainaph@gmail.com>
2024-11-28 07:25:58 -0300

    add translations

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -1,14 +1,16 @@
 'use client';
+import { useTranslations } from 'next-intl';
+
 export const CallToAction: React.FC = () => {
+  const t = useTranslations('CallToAction');
+
   return (
     <footer className="hidden md:block fixed bottom-0 w-full bg-indigo-600 text-white py-4 px-6 shadow-md z-50">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         {/* CTA Text */}
         <div className="text-center md:text-left mb-3 md:mb-0">
-          <h3 className="text-lg font-semibold">Ready to Revolutionize Your Business?</h3>
-          <p className="text-sm text-indigo-200">
-            Join the growing list of businesses transforming with AI.
-          </p>
+          <h3 className="text-lg font-semibold">{t('title')}</h3>
+          <p className="text-sm text-indigo-200">{t('description')}</p>
         </div>
 
         {/* Buttons */}
@@ -17,13 +19,13 @@ export const CallToAction: React.FC = () => {
             href="#get-started"
             className="bg-white flex items-center justify-center  text-indigo-600 px-6 py-2 rounded-lg text-sm font-semibold shadow-md hover:bg-indigo-100 transition"
           >
-            Get Started
+            {t('startButton')}
           </a>
           <a
             href="#contact-us"
             className="bg-transparent border-2 border-white px-6 py-2 rounded-lg text-sm font-semibold hover:bg-white hover:text-indigo-600 transition"
           >
-            Contact Us
+            {t('contactButton')}
           </a>
         </div>
       </div>
